f3c9a3daef5048ad198116aa9a6c492fedf2193a
E-SaiAnurath <2303a51560@sru.edu.in>
2026-02-10 23:07:25 +0530

    Add files via upload

---
+++ personal-finance-tracker-auth/frontend/Login.js
@@ -0,0 +1,60 @@
+import { useState } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+
+export default function Login() {
+  const navigate = useNavigate();
+  const [email, setEmail] = useState("user1@gmail.com");
+  const [password, setPassword] = useState("123456");
+  const [error, setError] = useState("not right");
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    try {
+      const res = await axios.post("http://localhost:5000/api/auth/login", {
+        email,
+        password,
+      });
+
+      const token = res.data.token;
+
+      // Save token
+      localStorage.setItem("token", token);
+
+      // Redirect
+      navigate("/dashboard");
+    } catch (err) {
+      setError("Invalid credentials");
+    }
+  };
+
+  return (
+    <div style={{ width: 300, margin: "50px auto" }}>
+      <h2>Login</h2>
+
+      {error && <p style={{ color: "red" }}>{error}</p>}
+
+      <form onSubmit={handleLogin}>
+        <input
+          type="email"
+          placeholder="Enter email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          required
+          style={{ width: "100%", marginBottom: 10 }}
+        />
+
+        <input
+          type="password"
+          placeholder="Enter password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          required
+          style={{ width: "100%", marginBottom: 10 }}
+        />
+
+        <button type="submit" style={{ width: "100%" }}>Login</button>
+      </form>
+    </div>
+  );
+}
