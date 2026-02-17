d5d7dafbc12fd4447439258a170246e90774d027
E-SaiAnurath <2303a51560@sru.edu.in>
2026-02-17 23:20:59 +0530

    Add files via upload

---
+++ personal-finace-tracker-db-ci/db.js
@@ -0,0 +1,15 @@
+Create *src/db.js*
+-----------------------
+const mongoose = require("mongoose");
+
+async function connectDB(uri) {
+  mongoose.set("strictQuery", true);
+  await mongoose.connect(uri);
+  return mongoose.connection;
+}
+
+async function disconnectDB() {
+  await mongoose.disconnect();
+}
+
+module.exports = { connectDB, disconnectDB };
